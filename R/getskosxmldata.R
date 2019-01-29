ad31e8ab94a53f7614e0d82f52322f5d9303ed54
Daphnis De Pooter <Daphnisd@vliz.be>
2019-01-29 14:11:00 +0100

    ad features

--- R/getskosxmldata.R
+++ R/getskosxmldata.R
@@ -2,32 +2,85 @@
 #'
 #' This function allows to get the information form different concepts stored in skoss xml. It takes a single concept, multiple concepts or a shema. The output is a list of 2 data frames; one with the lables and definitions. The other with the relations with other concepts.
 #' @param vocids mandatory parameter, the XML link(s)  you want to read the read
+#' @param vocabs optional parameter, include which vocabs you want to include. Takes multiple vocabs if separated by a "|" (eg. "P01|P06|Q01")
 #' @import dplyr xml2 stringr
 #' @export
 #' @examples
 #' getskossxmldata("http://vocab.nerc.ac.uk/collection/P06/current/UMCU/")
 #' getskossxmldata(c("http://vocab.nerc.ac.uk/collection/P06/current/UMCU/","http://vocab.nerc.ac.uk/collection/P06/current/ULAA/")
-#' getskossxmldata("http://vocab.nerc.ac.uk/collection/S10/current/")
+#' getskossxmldata("http://vocab.nerc.ac.uk/collection/S10/current/", vocabs ="S10|S11")
 #' getskossxmldata("http://dd.eionet.europa.eu/vocabulary/biodiversity/eunishabitats/rdf")
 
 
-getskossxmldata <- function (vocids) {
+getskossxmldata <- function (vocids = NA, vocabs = NA) {
+  
+  vocids <- unique(vocids)
+  
+  if(!is.na(vocabs)) {
+    vocids <- unique(subset (vocids,(grepl (vocabs, vocids)) & 
+                               (grepl ("http://vocab.nerc.ac.uk/collection", vocids) &
+                                  !grepl(glob2rx("*current?") , vocids) & 
+                                  !grepl(glob2rx("*current") , vocids))))
+  }  
+  
   for (i in vocids) {
     skossxml  <- readskossxml(i)
     terminfo <- skossxmlinfo(skossxml)
     termrelation <- skossxmlrelations(skossxml)
-
+    
     if (exists("terminfos")) {terminfos <- bind_rows(terminfos, terminfo)} else {  terminfos <- terminfo}
     if (exists("termrelations")) {termrelations <- bind_rows(termrelations, termrelation)} else {  termrelations <- termrelation}
   }
-
+  
   skossxmldata <-list()
   skossxmldata$termrelations <- termrelations
   skossxmldata$terminfos <- terminfos
-
+  
   return(skossxmldata)
-
+  
 }
+
+
+
+
+#' get skos xml dataterms
+#'
+#' This function allows to get the information form different concepts stored in skoss xml. It takes a single concept, multiple concepts or a shema. The output a dataframe with the lables and definitions. 
+#' @param vocids mandatory parameter, the XML link(s)  you want to read the read
+#' @param vocabs optional parameter, include which vocabs you want to include. Takes multiple vocabs if separated by a "|" (eg. "P01|P06|Q01")
+#' @import dplyr xml2 stringr
+#' @export
+#' @examples
+#' getskossxmldatainfo("http://vocab.nerc.ac.uk/collection/P06/current/UMCU/")
+#' getskossxmldatainfo(c("http://vocab.nerc.ac.uk/collection/P06/current/UMCU/","http://vocab.nerc.ac.uk/collection/P06/current/ULAA/")
+#' getskossxmldatainfo("http://vocab.nerc.ac.uk/collection/S10/current/", vocabs ="S10|S11")
+#' getskossxmldatainfo("http://dd.eionet.europa.eu/vocabulary/biodiversity/eunishabitats/rdf")
+
+
+
+getskossxmldatainfo <- function (vocids = NA, vocabs = NA) {
+  
+  vocids <- unique(vocids)
+  
+  if(!is.na(vocabs)) {
+    vocids <- unique(subset (vocids,(grepl (vocabs, vocids) & 
+                                       !grepl(glob2rx("*current?") , vocids) & 
+                                       !grepl(glob2rx("*current") , vocids))))
+  }  
+ 
+  terminfo<- lapply(vocids, function(x) {
+  skossxmlinfo(readskossxml(x))
+  } ) 
+  terminfos <- bind_rows(terminfo)                  
+                    
+ 
+  return(terminfos)
+}
+
+
+
+
+
 
 #' read skoss xml file
 #'
@@ -38,12 +91,16 @@ getskossxmldata <- function (vocids) {
 
 
 readskossxml <- function(X) {
-  x <- xml2::read_xml(X)
-  skossxml <- xml2::xml_find_all(x, ".//skos:Concept")
-
-  return(skossxml)
+ tryCatch({
+  x <- suppressWarnings(xml2::read_xml(X))
+  skossxml <- suppressWarnings(xml2::xml_find_all(x, ".//skos:Concept")) }
+  , error = function(x){
+  print(paste0(X," does not resolve to a concept"))
+  })
+if (exists("skossxml")){
+return(skossxml)
+} 
 }
-
 
 #' parse skoss xml file
 #'
@@ -53,21 +110,22 @@ readskossxml <- function(X) {
 #' @export
 
 
-skossxmlinfo <- function (skossxml) {
-
+skossxmlinfo <- function (skossxml = NA) {
+  if(!is.null(skossxml)) {if(!is.na(skossxml)){
+    
   l <- lapply(skossxml, function(term) {
     element <- xml2::as_list(term)
     return(list(
       identifier = as.character(if (length(unlist(element$identifier))>0) {unlist(element$identifier)} else {"NA"}),
-      prefLabel  = as.character(if (length(unlist(element$prefLabel ))>0) {unlist(element$prefLabel )} else {"NA"}),
-      altLabel  = as.character(if (length(unlist(element$altLabel ))>0) {unlist(element$altLabel )} else {"NA"}),
+      preflabel  = as.character(if (length(unlist(element$prefLabel ))>0) {unlist(element$prefLabel )} else {"NA"}),
+      altlabel  = as.character(if (length(unlist(element$altLabel ))>0) {unlist(element$altLabel )} else {"NA"}),
       definition = as.character(if (length(unlist(element$definition))>0) {unlist(element$definition)} else {"NA"}),
       deprecated = as.character(if (length(unlist(element$deprecated))>0) {unlist(element$deprecated)} else {"NA"}),
       uri = xml2::xml_attr(term, "about")))})
 
   terminfo <- bind_rows(l)
   return (terminfo)
-}
+}}}
 
 
 #' parse skoss xml file2
@@ -77,31 +135,31 @@ skossxmlinfo <- function (skossxml) {
 #' @import  xml2
 #' @export
 
+
 skossxmlrelations <- function (skossxml) {
+  if(!is.null(skossxml)) {if(!is.na(skossxml)){
+  
+    
+    termr2 <- lapply(skossxml, function(y) {
+    termr <-  lapply(skossconcepts, function(j) {
+        
+        options(stringsAsFactors = FALSE)
+        templ <- xml2::xml_find_all(skossxml,paste0(".//",j))
+        tempid <- xml2::xml_attr(skossxml, "about")
+        
+        if(length(templ)>0){
+          temp <- dplyr::bind_rows(lapply(xml2::xml_attrs(templ), function(x) data.frame(as.list(x), stringsAsFactors=FALSE)))
+          temp <- cbind(term = tempid ,temp,relation =richtfrom(j,":",0))
+          }
+        })
+      termr2 <-  bind_rows(termr)  
+    })
+    
+    termrelations <- bind_rows(termr2)
 
-  for (x in 1:length(skossxml)) {
-    for (j in skossconcepts) {
-      options(stringsAsFactors = FALSE)
-      templ <- xml2::xml_find_all(skossxml[x],paste0(".//",j))
-      tempid <- xml2::xml_attr(skossxml[x], "about")
+    return(termrelations)
+  }}}
 
-      if(length(templ)>0){
-        temp <- dplyr::bind_rows(lapply(xml2::xml_attrs(templ), function(x) data.frame(as.list(x), stringsAsFactors=FALSE)))
-        temp <- cbind(term = tempid ,temp,relation =richtfrom(j,":",0))
-
-        if (exists("termr")) {termr <- bind_rows(termr,temp ) } else {termr <- temp }
-        rm(temp)
-      }
-    }
-    if (exists("termr")){if(exists("termr2")) {termr2 <- bind_rows(termr2,termr) } else {termr2 <- termr}}
-    if (exists("termr")){rm(termr)}
-
-  }
-
-  if (exists("termr2")){
-    termrelations <- termr2 } else {termrelations <- data.frame(term = tempid, resource = NA, relation = NA) }
-  return(termrelations)
-}
 
 
 
---
+++ R/getunitsandparams.R
@@ -0,0 +1,71 @@
+#' get all related terms of a concept 
+#'
+#' This function retuns to you all related terms of one or more concept. The output is a vector with the related ids 
+#' @param vocid mandatory parameter, the XML link(s) of which you want the related concepts
+#' @param relation optional parameter, the type of the retation you want. It takes as values: "narrower", "broader", "related", "sameas" 
+#' @param vocab optional parameter, the vocab of the returned id's. For example: "P01", "P06" 
+#' @import dplyr 
+#' @export
+#' @examples
+#' getrelatedterms(vocid = "http://vocab.nerc.ac.uk/collection/S25/current/BE007117/", relation = "narrower", vocab = "P01" )
+
+
+
+getrelatedterms <- function (vocid=NA, relation= NA, vocab = NA ) {
+tryCatch({
+if (!is.na(relation) & !is.na(vocab)){
+relatedterms <- (getskossxmldata(vocid)$termrelation %>% filter (relation == relation,grepl(vocab, resource)))$resource
+} else {
+if (is.na(relation) & !is.na(vocab)){
+relatedterms <- (getskossxmldata(vocid)$termrelation %>% filter (grepl(vocab, resource)))$resource
+  
+} else {
+  if (!is.na(relation) & is.na(vocab)){
+relatedterms <- (getskossxmldata(vocid)$termrelation %>% filter (relation == relation))$resource
+} else {
+relatedterms <- (getskossxmldata(vocid)$termrelation)$resource
+}}}
+}, error = function(x) {print(past0(vocid ," does not resolve"))}) 
+if(exists("relatedterms")){return(relatedterms)  }
+}
+
+
+
+
+#' get both the parameterinfo and the unitinfo of a given concept 
+#'
+#' @param vocid mandatory parameter, the XML link(s) of which you want the related concepts.
+#' @param vocabs optional parameter, include which vocabs you want to include. Takes multiple vocabs if separated by a "|" (eg. "P01|P06|Q01")
+#' @import dplyr 
+#' @export
+#' @examples
+#' getunitsandparams(vocid = "http://vocab.nerc.ac.uk/collection/P01/current/OWETBM01/" )
+#' getunitsandparams(vocid = "http://vocab.nerc.ac.uk/collection/P01/current/OWETBM01/" , vocabs ="P01|Q01" )
+
+
+getunitsandparams <- function(vocids=NA, vocabs = NA) {
+  
+  
+  if(!is.na(vocabs)) {
+    vocids <- unique(subset (vocids,(grepl (vocabs, vocids)) & 
+                               (grepl ("http://vocab.nerc.ac.uk/collection", vocids) &
+                                  !grepl(glob2rx("*current?") , vocids) & 
+                                  !grepl(glob2rx("*current") , vocids))))
+  }  
+  
+  
+      df<- getskossxmldata(vocids)
+      if(length(df) >  0) {
+      
+      para <- df$terminfo %>% select (preflabel , altlabel, definition, deprecated, uri)
+      paraandunit <- df$termrelation %>% filter (relation == "related" & grepl("P06", resource)) %>% rename(standardUnitID =resource)
+      unit <- getskossxmldatainfo(unique(paraandunit$standardUnitID)) %>% select(standardUnitID = uri, unitpref = preflabel, standardunit = altlabel )
+    
+      if (nrow(paraandunit)>0){
+      parawithhunit <-  para %>% left_join(paraandunit, by =c( "uri" = "term" )) %>% left_join(unit, by = c("standardUnitID")) %>% distinct()
+      } else {parawithhunit <-  para}
+      return (parawithhunit) 
+  
+  }
+  }
+
